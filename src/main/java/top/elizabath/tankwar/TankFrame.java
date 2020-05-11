8face4400df8b17f56dcaddff274b5d23d48c7f4
lipu <lipu@lab.rhzz.com.cn>
2020-05-11 10:04:55 +0800

    123

--- src/main/java/top/elizabath/tankwar/TankFrame.java
+++ src/main/java/top/elizabath/tankwar/TankFrame.java
@@ -1,16 +1,21 @@
 package top.elizabath.tankwar;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
 
+    int x = 200, y = 200;
+
     public TankFrame() throws HeadlessException {
-        this.setSize(800,600);
+        this.setSize(800, 600);
         this.setResizable(false);
         this.setTitle("坦克大战！");
         this.setVisible(true);
+        this.addKeyListener(new MyKeyListener());
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -22,6 +27,20 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g) {
         System.out.println("paint");
-        g.fillRect(200,200,50,50);
+        g.fillRect(x, y, 50, 50);
+        x += 20;
+        y += 20;
+    }
+
+    static class MyKeyListener extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("keyPressed");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("keyReleased");
+        }
     }
 }
